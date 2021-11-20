f2f7ab7dada010a3895df42ce54695398ff69d33
Alexandre Bekor <contato@alexandrebekor.com>
2021-11-19 23:04:01 -0300

    Include and config initial gatsby

---
+++ src/components/Banner/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as styles from './styles.module.css'
+
+const Banner = () => {
+    return (
+        <p className={styles.text}>Testando</p>
+    )
+}
+
+export default Banner
---
+++ src/pages/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Banner from '../components/Banner'
+
+const Home = () => {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Banner />
+        </>
+    )
+}
+export default Home
